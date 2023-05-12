e259784042dd2d55c72f41c6fd0d72dc3585ea1b
Alfredo Zimperz <17734680+fredyzz@users.noreply.github.com>
2023-05-12 19:34:14 +0200

    Create SubmitButton

---
+++ src/components/submitButton/index.jsx
@@ -0,0 +1,24 @@
+import PropTypes from "prop-types";
+
+import { NOOP } from "@/utils/constants.js";
+
+const SubmitButton = ({ disabled, onClick = NOOP }) => {
+  const handleOnClick = () => onClick();
+
+  return (
+    <button
+    className="w-full mt-4 p-3 text-sm font-medium text-white bg-blue-600 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 hover:cursor-pointer disabled:bg-gray-400 disabled:cursor-not-allowed"
+      disabled={disabled}
+      onClick={handleOnClick}
+    >
+      Submit
+    </button>
+  );
+};
+
+SubmitButton.propTypes = {
+  disabled: PropTypes.bool,
+  onClick: PropTypes.func,
+};
+
+export default SubmitButton;
