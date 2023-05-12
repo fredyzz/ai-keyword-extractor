76cda2516018dc7eba818e29151a1fe7e4c74404
Alfredo Zimperz <17734680+fredyzz@users.noreply.github.com>
2023-05-12 19:34:53 +0200

    Create TextInput

--- src/components/textInput/index.jsx
+++ src/components/textInput/index.jsx
@@ -11,9 +11,18 @@ const TextInput = ({onChange = NOOP}) => {
     onChange(e.target.value);
   }
 
-  return (
-    <textarea rows="5" cols="33" value={text} onChange={handleOnChange}/>
-  );
+  return <>
+    <label htmlFor="text" className="block text-sm font-medium text-gray-700 mb-1">
+    Text
+  </label>
+  <textarea
+    id="text"
+    value={text}
+    onChange={handleOnChange}
+    className="p-3 w-full h-24 border border-gray-300 rounded-md focus:outline-none focus:border-blue-300 focus:ring-1 focus:ring-blue-300"
+    placeholder="Enter text to extract keywords"
+  />
+  </>;
 };
 
 TextInput.propTypes = {onChange: PropTypes.func};
