1d8058b6281dfdd1750347bcfad21b45b8e6a677
Alfredo Zimperz <17734680+fredyzz@users.noreply.github.com>
2023-05-13 14:40:33 +0200

    Add textOutput constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,11 @@
 export const NOOP = () => {};
 
 export const OPENAI_API_URL = "https://api.openai.com/v1/completions";
+
+export const TEXT_OUTPUT_TYPES = {
+  ARRAY: "Array",
+  COMMA_SEPARATED: "Comma Separated",
+  SPACE_SEPARATED: "Space Separated",
+};
+
+export const TEXT_OUTPUT_TYPES_DEFAULT = TEXT_OUTPUT_TYPES.ARRAY;
